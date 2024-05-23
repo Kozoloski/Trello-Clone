2da99e9c122ea97dba5c1035bcd125537fe8b440
Kozoloskit <kozoloski@yahoo.com>
2024-05-23 20:47:37 +0200

    Starting with Sidebar Layout

--- trello-clone/middleware.ts
+++ trello-clone/middleware.ts
@@ -1,30 +1,7 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware, } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ["/"],
-  afterAuth(auth, req) {
-    if(auth.userId && auth.isPublicRoute) {
-      let path = "/select-org";
 
-      if (auth.orgId) {
-        path =  `/organization/${auth.orgId}`;
-      }
-
-      const orgSelection = new URL(path, req.url);
-      return NextResponse.redirect(orgSelection);
-    }
-
-    if(!auth.userId && !auth.isPublicRoute) {
-      return this.redirectToSignIn ({ returnBackUrl: req.url });
-    }
-
-    if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
-      const orgSelection = new URL("/select-rtg", req.url);
-      return NextResponse.redirect(orgSelection);
-    }
-  }
-});
+export default clerkMiddleware();
 
 
 
