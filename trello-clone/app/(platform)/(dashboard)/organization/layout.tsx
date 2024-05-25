c3b728270830f13670b47b921d977703b3736e33
Kozoloskit <kozoloski@yahoo.com>
2024-05-25 23:01:57 +0200

    Editing the Sidebar

--- trello-clone/app/(platform)/(dashboard)/organization/layout.tsx
+++ trello-clone/app/(platform)/(dashboard)/organization/layout.tsx
@@ -6,7 +6,10 @@ const OrganizationLayout = ({
     return (
         <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:mx-screen-xl
         mx-auto">
-            <div>
+            <div className="flex gap-x-7">
+            <div className="w-64 shrink-0 hidden md:block">
+                
+            </div>
               {children}
             </div>
         </main>
