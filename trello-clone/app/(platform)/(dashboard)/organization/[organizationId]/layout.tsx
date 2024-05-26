a2b87fa4d0955eff9e31a3552a679dd033524b20
Kozoloskit <kozoloski@yahoo.com>
2024-05-26 20:50:05 +0200

    Created OrganizationIdLayout File

---
+++ trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,12 @@
+const OrganizationIdLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <>
+            <OrgControl/>
+            {children}      
+        </>
+    );
+};
