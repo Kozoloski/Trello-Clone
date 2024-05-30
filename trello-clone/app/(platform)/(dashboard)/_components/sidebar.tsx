2faf7812719b54076394085c3813f302aa69ba99
Kozoloskit <kozoloski@yahoo.com>
2024-05-30 23:06:56 +0200

    Editing the Sidebar Component

--- trello-clone/app/(platform)/(dashboard)/_components/sidebar.tsx
+++ trello-clone/app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+
+import { Button} from "@/components/ui/button";
+
 interface SidebarProps {
     storageKey?:string;
 };
 
-export const Sidebar = () => {
+export const Sidebar = ({
+    storageKey,
+}: SidebarProps) => {
     return (
         <div>
             Sidebar!
